7f62f1f7dd060af324ab4912f6de9162264dcf0c
Rehan Javed <rehanjaved.official@gmail.com>
2018-05-23 01:23:01 +0500

    Sound and Search Bar Implementation
    Sound of button on clicking the pokemon. Search bar is implemented to get the filtered items.

--- Pokemon Detailer/Pokemon Detailer/Controllers/PokemonController.swift
+++ Pokemon Detailer/Pokemon Detailer/Controllers/PokemonController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import AVKit
 
-class PokemonController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class PokemonController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     // Attributes..
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
     private var pokemonData: [Pokemon] = []
+    private var filteredPokemonData: [Pokemon] = []
+    private var buttonClick: AVAudioPlayer!
+    private var searchedData: Bool = false
     
     // When view in complete.
     override func viewDidLoad() {
@@ -20,8 +25,30 @@ class PokemonController: UIViewController, UICollectionViewDelegate, UICollectio
         
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        self.searchBar.delegate = self
         
+        self.searchBar.returnKeyType = UIReturnKeyType.done
         self.pokemonData = CSVReader().readCSVData() // reading the data from csv file..
+        
+        initClickSound()
+        
+    }
+    
+    // initializing sound..
+    func initClickSound(){
+        
+        do{
+           
+            let path = Bundle.main.path(forResource: "buttonpressed", ofType: "mp3")!
+            let encoded = path.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed)!
+            buttonClick = try AVAudioPlayer(contentsOf: URL(string: encoded)!)
+            buttonClick.prepareToPlay()
+            buttonClick.numberOfLoops = 1
+            
+        }catch {
+            print("Error occured while initializing Sound..")
+        }
+        
         
     }
 
@@ -31,7 +58,17 @@ class PokemonController: UIViewController, UICollectionViewDelegate, UICollectio
     
     // number of pokemons..
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pokemonData.count
+        
+        if searchedData {
+            
+            return filteredPokemonData.count
+            
+        } else {
+            
+            return pokemonData.count
+            
+        }
+        
     }
     
     // cell in the section.
@@ -39,7 +76,12 @@ class PokemonController: UIViewController, UICollectionViewDelegate, UICollectio
         
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pokemoncell", for: indexPath) as? PokemonCell {
             
-            cell.registerPokemon(pokemon: self.pokemonData[indexPath.row])
+            if searchedData {
+                cell.registerPokemon(pokemon: self.filteredPokemonData[indexPath.row])
+            } else {
+                cell.registerPokemon(pokemon: self.pokemonData[indexPath.row])
+            }
+            
             return cell
             
         }else{
@@ -50,17 +92,71 @@ class PokemonController: UIViewController, UICollectionViewDelegate, UICollectio
     
     // number of sections..
     func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
         return 1
+    
     }
 
     // when any cell is clicked..
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("\(self.pokemonData[indexPath.row].getPokemonName), \(self.pokemonData[indexPath.row].getPokemonID)")
+        
+        self.buttonClick.play()
+    
+        if searchedData {
+            
+            print("\(self.filteredPokemonData[indexPath.row].getPokemonName), \(self.filteredPokemonData[indexPath.row].getPokemonID)")
+            
+        }else {
+            
+            print("\(self.pokemonData[indexPath.row].getPokemonName), \(self.pokemonData[indexPath.row].getPokemonID)")
+            
+        }
+    
     }
     
     // the size of each pokemon cell.
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
         return CGSize(width: 115, height: 130)
+    
+    }
+    
+    ///////////////
+    // Search Bar
+    ///////////////
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText == "" {
+            
+            searchedData = false
+            searchBar.endEditing(true)
+            collectionView.reloadData()
+            
+        }else{
+        
+            let searchedText = searchText.lowercased()
+            searchedData = true
+            filteredPokemonData = pokemonData.filter({$0.getPokemonName.range(of: searchedText) != nil})
+            collectionView.reloadData()
+            
+        }
+        
+    }
+    
+    // When done button is clicked on keyboard to close it.
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+        searchBar.endEditing(true)
+        
+    }
+    
+    // when cancel button is pressed in Search Bar.
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchedData = false
+        searchBar.text = ""
+        collectionView.reloadData()
+        
     }
     
 }
